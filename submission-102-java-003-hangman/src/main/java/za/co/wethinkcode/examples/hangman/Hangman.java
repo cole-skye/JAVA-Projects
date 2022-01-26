2ef19d1610bcc9673b8b6563ae8d51e54ac9f70b
cole-skye <colesf@icloud.com>
2022-01-26 10:15:40 +0200

    hangman

---
+++ submission-102-java-003-hangman/src/main/java/za/co/wethinkcode/examples/hangman/Hangman.java
@@ -0,0 +1,68 @@
+package za.co.wethinkcode.examples.hangman;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+// tag::hangman-class[]
+public class Hangman {
+    private static Answer wordToGuess;
+
+    public static void main(String[] args) throws IOException {
+
+        Player player = new Player();
+        Answer wordToGuess = pickWord(player);
+        Answer currentAnswer = start(player,wordToGuess);
+        run(player, wordToGuess, currentAnswer);
+
+    }
+
+    private static void run(Player player, Answer wordToGuess, Answer currentAnswer) {
+        while (!currentAnswer.equals(wordToGuess)) {
+            String guess = player.getGuess();
+            if (player.wantsToQuit()) {
+                System.out.println("Bye!");
+                break;
+            }
+
+            char guessedLetter = guess.charAt(0);
+            if (currentAnswer.isGoodGuess(wordToGuess, guessedLetter)) {
+                currentAnswer = wordToGuess.getHint(currentAnswer, guess.charAt(0));
+                System.out.println(currentAnswer);
+            } else {
+                player.lostChance();
+                System.out.println("Wrong! Number of guesses left: " + player.getChances());
+                if (player.hasNoChances()) {
+                    System.out.println("Sorry, you are out of guesses. The word was: " + wordToGuess);
+                    break;
+                }
+            }
+        }
+        if (currentAnswer.equals(wordToGuess)) {
+            System.out.println("That is correct! You escaped the noose .. this time.");
+        }
+    }
+
+    private static Answer pickWord(Player player) throws IOException {
+        Random random = new Random();
+
+        System.out.println("Words file? [leave empty to use short_words.txt]");
+        String fileName = player.getWordsFile();
+
+        List<String> words = Files.readAllLines(Path.of(fileName));
+
+        int randomIndex = random.nextInt(words.size());
+        String randomWord = words.get(randomIndex).trim();
+        return new Answer(randomWord);
+    }
+
+    private static Answer start(Player player, Answer wordToGuess) {
+        Answer currentAnswer = wordToGuess.generateRandomHint();
+        System.out.println("Guess the word: " + currentAnswer);
+        return currentAnswer;
+    }
+
+}
